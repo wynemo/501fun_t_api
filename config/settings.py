ebabd44480271ddcc41fe7ef28fafda5320d1efc
wynemo <ggdabin@gmail.com>
2012-04-24 14:30:18 +0000

    first commit

---
+++ config/settings.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+import web, os
+
+def get_home_dir():
+    try:
+        dir1 = os.environ['OPENSHIFT_REPO_DIR']
+        if dir1.endswith('/') == False:
+            dir1 += '/'
+        dir1 += 'wsgi/'
+        return dir1
+    except:
+        return './'
+
+render = web.template.render(get_home_dir() + 'templates/', cache=False)
+
+web.config.debug = False
+
+consumer_key = ""
+consumer_secret = ""
+
+#for j.mp link
+no_jmp = False 
+bitly_name = ''
+bitly_key = ''
+
