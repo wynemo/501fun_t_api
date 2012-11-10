ad3c810820c6697a14d2955232e3763597e2a849
wynemo <ggdabin@gamil.com>
2012-11-10 10:53:44 +0000

    api no version ,default to 1

--- twitterapi/oauthtwitter.py
+++ twitterapi/oauthtwitter.py
@@ -188,6 +188,9 @@ class OAuthApi():
         if parameters.has_key('pc'):parameters.pop('pc')
         if parameters.has_key('send_error_codes'):parameters.pop('send_error_codes')
 
+        if not call.startswith('1/') and not call.startswith('i/') and \
+            not call.startswith('1.1/'):
+            call = '1/' + call
         logging.error('https://api.twitter.com/' + call + ' ' + str(parameters))
         data1 = self._FetchUrl('https://api.twitter.com/' + call , type, parameters)
         return data1 
